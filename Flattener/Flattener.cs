f40a68b03d3075b6bef889688c8e50abf99cb578
Mukul Khanna <mukulkhanna.96@gmail.com>
2023-09-22 19:33:55 +0530

    Initial commit and added interface for the base class

---
+++ Flattener/Flattener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flattener;
+
+public class Flattener : IFlattener
+{
+    public IDictionary<string, string> Flatten(IDictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDictionary<string, T?> Flatten<T>(IDictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Flattener/IFlattener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flattener;
+
+public interface IFlattener
+{
+    IDictionary<string, string> Flatten(IDictionary<string, object> data);
+    IDictionary<string, T?> Flatten<T>(IDictionary<string, object> data);
+}
